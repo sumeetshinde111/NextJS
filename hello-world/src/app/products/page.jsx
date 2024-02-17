16be2891288452e0bad69e295df194ae26d9c1e4
sumeet shinde <sumeet.shinde@springernature.com>
2024-02-17 22:31:03 +0530

    create new routes in next.js

---
+++ hello-world/src/app/products/page.jsx
@@ -0,0 +1,12 @@
+
+const products = ()=>{
+    return(
+        <>
+            <h1>This is a product page!!</h1>
+            <div>
+                In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.
+            </div>
+        </>
+    )
+}
+export default products;
---
+++ hello-world/src/app/userlist/page.tsx
@@ -0,0 +1,15 @@
+
+const userList = ()=>{
+    return(
+        <>
+            <ul>
+                <li>User 1</li>
+                <li>User 2</li>
+                <li>User 3</li>
+                <li>User 4</li>
+
+            </ul>
+        </>
+    )
+}
+export default userList;
